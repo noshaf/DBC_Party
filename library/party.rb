e2782c8848708b11f0203e41d7a9710f2cc0d1de
noshaf <noshaf@gmail.com>
2012-07-17 21:22:15 -0700

    Last commit of the day just a couple tests added. Leaving on Red.

--- library/party.rb
+++ library/party.rb
@@ -1,8 +1,9 @@
 class Party
 
-  attr_reader :music
-  def initialize(dir)
+  attr_reader :music, :name
+  def initialize(dir, name)
     @music = Dir.entries(dir)[2..-1]
+    @name = name
   end
 
 end
--- spec/party_spec.rb
+++ spec/party_spec.rb
@@ -8,18 +8,24 @@ describe Party do
   describe '#initialize' do
 
     before :each do
-      @party = Party.new("test_music")
+      @party = Party.new("test_music", "Noah's Party")
     end
 
-    it "takes one argument" do
+    it "takes two argument" do
       expect{
       @party
       }.should_not raise_error(ArgumentError)
     end
 
-    it "takes a directory of mp3 files as the argument" do
+    it "takes a directory of mp3 files as the first argument" do
       @party.music.first.split('.')[1].should eq 'mp3'
     end
+
+    it "has a name as the second argument" do
+      @party.name.should eq "Noah's Party"
+    end
+
+    it "plays "
 
   end
 
