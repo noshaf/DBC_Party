8e01ba815b1e159d93aec4af996b2fe71dd14865
viceversus <apprentice@devbootcamp.com>
2012-07-18 20:44:06 -0700

    Spotify API tapped to qurey names

--- spec/spotify_spec.rb
+++ spec/spotify_spec.rb
@@ -4,7 +4,12 @@ require 'rspec'
 require '../library/spotify.rb'
 
 include Spotify
-describe Spotify do 
-  
-  
+describe Spotify do
+
+  context "#call" do
+
+    it "calls the spotify API with a query" do
+      call("Usher").first.should eq "Without You - feat. Usher"
+    end
+
 end
